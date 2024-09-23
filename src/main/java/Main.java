fec2b2ae246dae1452cd37e3e482e8c7df353e2b
Berik2912 <bktanov@mail.com>
2024-09-23 21:16:47 +0600

    solved the problem :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,65 @@
+import java.util.Scanner;
+
+class Car {
+    String name;
+    int speed;
+
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+class Race {
+    String leaderName = "";
+    int leaderDistance = 0;
+
+    public void updateLeader(Car car) {
+        int distance = car.getSpeed() * 24;
+        if (distance > leaderDistance) {
+            leaderName = car.getName();
+            leaderDistance = distance;
+        }
+    }
+
+    public String getLeaderName() {
+        return leaderName;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название машины №" + i + ":");
+            String name = scanner.next();
+
+            int speed = 0;
+            while (true) {
+                System.out.println("Введите скорость машины №" + i + ":");
+                speed = scanner.nextInt();
+                if (speed > 0 && speed <= 250) {
+                    break;
+                } else {
+                    System.out.println("Неправильная скорость. Введите значение от 1 до 250.");
+                }
+            }
+
+            Car car = new Car(name, speed);
+            race.updateLeader(car);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.getLeaderName());
+        scanner.close();
     }
 }
